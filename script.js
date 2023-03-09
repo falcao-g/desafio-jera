be2793a5eb6d3a0b935b71cf9144436cd9a59022
Falcão <gabriel.p.falcao50@gmail.com>
2023-03-08 20:36:40 -0400

    adicionado background dinâmico e destaque de botão
    o background agora vai automaticamente mudar dependendo se for pomodoro ou pausa, os botões também vão ficar destacado com base no estado atual

--- script.js
+++ script.js
@@ -6,6 +6,10 @@ let interval
 let pomodoros = 0
 let isPaused = false
 let state = "pomodoro"
+const pomodoroColor = "coral"
+const pomodoroDarkerColor = "darkred"
+const pauseColor = "cadetblue"
+const pauseDarkerColor = "darkblue"
 
 const minutesSpan = document.getElementById("minutes")
 const secondsSpan = document.getElementById("seconds")
@@ -13,7 +17,11 @@ const intervalButton = document.getElementById("interval-button")
 const longIntervalButton = document.getElementById("long-interval-button")
 const configButton = document.getElementById("config")
 const pauseButton = document.getElementById("pause-button")
+const pomodoroButton = document.getElementById("pomodoro-button")
 const pomodorosSpan = document.getElementById("pomodoros")
+const h1 = document.querySelector("h1")
+const config = document.getElementById("config")
+const box = document.getElementById("box")
 
 function formatTime(time) {
 	const minutes = Math.floor(time / 60)
@@ -23,7 +31,22 @@ function formatTime(time) {
 		.padStart(2, "0")}`
 }
 
+function changeColor() {
+	if (state == "pomodoro") {
+		document.body.style.backgroundColor = pomodoroColor
+		h1.style.color = pomodoroDarkerColor
+		config.style.backgroundColor = pomodoroDarkerColor
+		box.style.backgroundColor = pomodoroDarkerColor
+	} else {
+		document.body.style.backgroundColor = pauseColor
+		h1.style.color = pauseDarkerColor
+		config.style.backgroundColor = pauseDarkerColor
+		box.style.backgroundColor = pauseDarkerColor
+	}
+}
+
 function startTimer() {
+	changeColor()
 	if (state == "pomodoro") {
 		time = pomodoroTime
 	} else if (state == "pause") {
@@ -43,14 +66,20 @@ function startTimer() {
 
 				if (state == "pomodoro") {
 					state = "pause"
+					pomodoroButton.classList.remove("highlight")
+					intervalButton.classList.add("highlight")
+					longIntervalButton.classList.remove("highlight")
+
+					pomodoros++
+					pomodorosSpan.textContent = pomodoros
 				} else {
 					state = "pomodoro"
+					pomodoroButton.classList.add("highlight")
+					intervalButton.classList.remove("highlight")
+					longIntervalButton.classList.remove("highlight")
 				}
 
 				startTimer()
-
-				pomodoros++
-				pomodorosSpan.textContent = pomodoros
 			}
 			minutesSpan.textContent = formatTime(time).split(":")[0]
 			secondsSpan.textContent = formatTime(time).split(":")[1]
@@ -58,13 +87,28 @@ function startTimer() {
 	}, 1000)
 }
 
+pomodoroButton.addEventListener("click", () => {
+	pomodoroButton.classList.add("highlight")
+	intervalButton.classList.remove("highlight")
+	longIntervalButton.classList.remove("highlight")
+	state = "pomodoro"
+	clearInterval(interval)
+	startTimer()
+})
+
 intervalButton.addEventListener("click", () => {
+	pomodoroButton.classList.remove("highlight")
+	intervalButton.classList.add("highlight")
+	longIntervalButton.classList.remove("highlight")
 	state = "pause"
 	clearInterval(interval)
 	startTimer()
 })
 
 longIntervalButton.addEventListener("click", () => {
+	pomodoroButton.classList.remove("highlight")
+	intervalButton.classList.remove("highlight")
+	longIntervalButton.classList.add("highlight")
 	state = "longpause"
 	clearInterval(interval)
 	startTimer()
